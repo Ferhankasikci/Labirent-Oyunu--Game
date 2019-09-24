a8621e6131e021a0c703bd38a251e6fc8ef79975
adem ozkan <ademozkn1@gmail.com>
2019-09-23 21:39:43 -0500

    Add project files.

---
+++ WFALAbirent/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WFALAbirent
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_MouseEnter(object sender, EventArgs e)
+        {
+            MessageBox.Show("Kaybettiniz!");
+            Application.Exit();
+        }
+
+        private void pictureBox1_MouseEnter(object sender, EventArgs e)
+        {
+            MessageBox.Show("Kazandınız!");
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Label5_MouseEnter(object sender, EventArgs e)
+        {
+            MessageBox.Show("Kaybettiniz!");
+            Application.Exit();
+        }
+    }
+}
